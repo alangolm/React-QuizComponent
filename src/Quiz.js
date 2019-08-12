3dc41fecedc59535effc94a94c02cadfda33d872
Alan <golman.alan@gmail.com>
2019-08-12 15:44:51 -0400

    refactored handler methods

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
     super(props)
     this.state = {quiz_position:1}
   }
-  showNextQuestion = () => {
+  showNextQuestion() {
     this.setState((state) => {
       return {quiz_position: state.quiz_position + 1}
     })
@@ -20,7 +20,7 @@ class Quiz extends Component {
       <div>
         {isQuizEnd ? <QuizEnd /> :
         <QuizQuestion quiz_question={quizData.quiz_questions
-        [this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion} />}
+        [this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
